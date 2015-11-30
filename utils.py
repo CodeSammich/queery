75fc1af06ec8df52a6462fa5723096af4b072914
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2015-11-29 22:35:32 -0500

    whenQuery is weird

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import google, urllib2, bs4, re
 
-def whoquery(s):
+def whoQuery(s):
     """
-    TL;DR: Searches input on google.
+    TL;DR: Searches input on google for "who" questions.
 
     Arguments:
     s (string) - A query string; what you are searching for.
@@ -40,6 +40,36 @@ def whoquery(s):
         i+=2
     return ans
 
+def whenQuery(s):
+    """
+    TL;DR: Searches input on google for "when" questions.
+
+    Arguments:
+    s (string) - A query string; what you are searching for.
+       -Ex. "when did World War I start"
+
+    Returns:
+    A list of names (strings) from the html of the top 5 google search results from the query.
+    """
+    results = google.search(s, num = 5, start = 0, stop = 5)
+
+    r = []
+    for pg in results:
+        r.append(pg)
+
+    dates = []
+    for n in range (0,9):
+        url = urllib2.urlopen(r[n])
+        page = url.read()
+        soup = bs4.BeautifulSoup(page, "html.parser")
+        raw = soup.get_text()
+        text = re.findall("\d{2}(-|\/)?\d{2}(-|\/)?\d+", raw)
+        for dict in text:
+            for i in dict:
+                if i != unicode('') and i != unicode(' ') and i != unicode('\n'):
+                    dates.append(i)
+    return dates
+
 def mostPopular(results):
     """
     TL;DR: Goes through list of names to find most frequent.
@@ -64,6 +94,9 @@ def mostPopular(results):
     
     return retString
 
-#a = whoquery("who played Spiderman")
+#a = whoQuery("who played Spiderman")
 #print a
 #print mostPopular(a)
+#b = whenQuery("when did World War I start")
+#print b
+#print mostPopular(b)
